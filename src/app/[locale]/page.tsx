0953c9bf1ddea4dc7e2bec62261087662634e111
Nruzhzhaugash <clonn262@gmail.com>
2024-02-24 11:33:20 +0600

    make pages

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,6 @@
 import HomePages from "@/pages/Home/Home";
-import { useTranslations } from "next-intl";
 
 export default function Home() {
-  const t = useTranslations("Index");
-
   return (
     <main>
       <HomePages />
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,16 @@
+import { useTranslations } from 'next-intl'
 import React from 'react'
 
 export default function HomePages() {
+  const t = useTranslations("IndexPage");
+  const hello = useTranslations("Index")
+
   return (
-    <div>Home</div>
+    <div>
+      {hello('title')}
+      <h1>{t('title')}</h1>
+      <p>{t('description')}</p>
+    </div>
   )
 }
 
