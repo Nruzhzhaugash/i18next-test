5b25abc47b26b2aca71116e4cb07877f6efe052a
Nruzhzhaugash <clonn262@gmail.com>
2024-02-24 17:24:44 +0600

    fix

--- src/widgets/Example/ui/Example.tsx
+++ src/widgets/Example/ui/Example.tsx
@@ -8,9 +8,9 @@ export default function Example() {
     <div className="">
       <h1 className="text-center text-4xl font-semibold mb-3">{t("title")}</h1>
       <div className="flex justify-center items-center gap-5">
-        <Link href='/about'>About</Link>
-        <Link href='/blog'>Blog</Link>
-        <Link locale="en" href='/'>Just</Link>
+        <Link href='/about' className="hover:underline">About</Link>
+        <Link href='/blog' className="hover:underline">Blog</Link>
+        <Link locale="en" href='/' className="hover:underline">Just</Link>
       </div>
     </div>
   )
